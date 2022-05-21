02073746796d61e5a967643689e2dc497e47d717
linct <test@testserver.com>
2022-05-21 13:00:05 -0500

    env based config, and update to dns entries

--- scratchpad/bytes_tests.py
+++ scratchpad/bytes_tests.py
@@ -1,5 +1,5 @@
 import codecs
 # todo make this work
 byte_str = '000a010000010000000000000178027979037a7a7a0000010001'
-plain = codecs.decode(byte_str,'hex')
+plain = codecs.decode(byte_str, 'hex')
 print(plain)
